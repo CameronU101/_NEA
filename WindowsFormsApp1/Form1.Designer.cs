69c0c7c69bddf8d86960b17d444d2e9ae67eb42a
Cameron Unsted <Cameron.Unsted@worthing.ac.uk>
2020-02-13 09:35:05 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class LoginFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameTxt = new System.Windows.Forms.TextBox();
+            this.PasswordTxt = new System.Windows.Forms.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UsernameTxt
+            // 
+            this.UsernameTxt.Location = new System.Drawing.Point(30, 46);
+            this.UsernameTxt.Name = "UsernameTxt";
+            this.UsernameTxt.Size = new System.Drawing.Size(165, 20);
+            this.UsernameTxt.TabIndex = 0;
+            this.UsernameTxt.Text = "Username";
+            this.UsernameTxt.TextChanged += new System.EventHandler(this.UsernameTxt_TextChanged);
+            this.UsernameTxt.Enter += new System.EventHandler(this.UsernameTxt_Entering);
+            // 
+            // PasswordTxt
+            // 
+            this.PasswordTxt.Location = new System.Drawing.Point(30, 84);
+            this.PasswordTxt.Name = "PasswordTxt";
+            this.PasswordTxt.Size = new System.Drawing.Size(165, 20);
+            this.PasswordTxt.TabIndex = 1;
+            this.PasswordTxt.Text = "Password";
+            this.PasswordTxt.TextChanged += new System.EventHandler(this.PasswordTxt_TextChanged);
+            this.PasswordTxt.Enter += new System.EventHandler(this.PasswordTxt_Entering);
+            this.PasswordTxt.Leave += new System.EventHandler(this.PasswordTxt_Leave);
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(30, 129);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoginBtn.TabIndex = 2;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(120, 129);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 3;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter your login details:";
+            // 
+            // LoginFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 167);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.PasswordTxt);
+            this.Controls.Add(this.UsernameTxt);
+            this.Name = "LoginFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UsernameTxt;
+        private System.Windows.Forms.TextBox PasswordTxt;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class LoginFrm : Form
+    {
+        public bool TextEmpty = true;
+
+        public LoginFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void PasswordTxt_TextChanged(object sender, EventArgs e)
+        {
+            
+                       
+        }
+
+        private void PasswordTxt_Entering(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(PasswordTxt.Text) || TextEmpty == true)  
+            {
+                PasswordTxt.Clear(); // When the text box is clicked on the sample text inside will disappear
+            }
+            PasswordTxt.PasswordChar = '*'; //This line make the User's input in the password textbox appear as *'s 
+        }
+
+        private void PasswordTxt_Leave(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(PasswordTxt.Text))
+            {
+                PasswordTxt.PasswordChar = '\0';
+                PasswordTxt.Text = "Password";
+                TextEmpty = true;
+            }
+
+            else
+            {
+                TextEmpty = false;
+            }
+                        
+
+        }
+
+        private void UsernameTxt_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+    private void UsernameTxt_Entering(object sender, EventArgs e)
+        {
+            UsernameTxt.Clear(); // When the text box is clicked on the sample text inside will disappear
+        }
+    }
+}
